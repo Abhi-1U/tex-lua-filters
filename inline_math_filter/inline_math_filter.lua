3738f6fd2943bbbd5dd382fb2704ddceaf207cac
Abhi-1U <62772223+Abhi-1U@users.noreply.github.com>
2022-08-10 09:37:58 +0530

    inline math filter

---
+++ inline_math_filter/inline_math_filter.lua
@@ -0,0 +1,20 @@
+--[[
+Inline Math Filter – Supports nested math environment for MathjaX V3 compatibility
+pandoc generated sample : $\mbox{$\mathbf B$}$
+filter generated equivalent : [\(\mbox{$\mathbf B$}\)]{.math .inline}
+Conversion type : LaTeX --> Markdown
+Copyright: © 2022 Abhishek Ulayil
+License:   MIT – see LICENSE file for details
+--]]
+
+--[[
+Applies the filter to Math elements
+--]]
+function Math(el)
+    if el.mathtype == 'InlineMath' then
+        local math_data =  "[\\(" .. el.text .. "\\)]{.math .inline}"
+        return(pandoc.RawInline('markdown', math_data))
+    else 
+        return(el)
+    end
+end
