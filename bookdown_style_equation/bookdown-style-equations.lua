5a49eb0fa41273f82d71f7279a2125e191a329d8
Abhi-1U <62772223+Abhi-1U@users.noreply.github.com>
2022-08-24 15:05:08 +0530

    Fixed Bookdown references and period incompatibility

--- bookdown_style_equation/bookdown-style-equations.lua
+++ bookdown_style_equation/bookdown-style-equations.lua
@@ -7,7 +7,8 @@ Conversion type : LaTeX --> Markdown
 Copyright: © 2022 Abhishek Ulayil
 License:   MIT – see LICENSE file for details
 --]]
-
+-- table to store all equation labels
+equation_labels = {}
 --[[
 Converts LaTeX label to bookdown label format in the Math element
 --]]
@@ -15,9 +16,37 @@ function Math(el)
     if el.text:match('label') then
         local text = pandoc.utils.stringify(el.text)
         s, e, l =string.find(text,"\\label{(.-)}")
+        table.insert(equation_labels,l)
+        -- Bookdown does not support period in equations hence substituting them as hyphen
+        l = string.gsub(l, "%.", "-")
         el.text = text .. [[(\#]] .. l .. [[)]]
     else
         --pass
     end
     return(el)
+end
+
+--[[
+Converts Links to equations to modified format
+--]]
+function Link(el)
+    for _,label in pairs(equation_labels) do
+        if ("#"..label) == el.target then
+            print(label)
+            local link_text = el.target
+            -- Bookdown does not support period in equations hence substituting them as hyphen
+            link_text = string.gsub(link_text, "%.", "-")
+            label = string.gsub(label, "%.", "-")
+            el.target = link_text
+            --bookdown type reference constructor
+            bkdwn = [[\@ref(]] .. label .. [[)]]
+            is_bkdwn = true
+            break
+        end
+    end
+    if is_bkdwn then
+        return pandoc.RawInline('markdown', bkdwn)
+    else
+        return(el)
+    end
 end
